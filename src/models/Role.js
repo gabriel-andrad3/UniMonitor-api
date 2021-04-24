9b5ab117150d70dade4732ff8c0c3de9123d22f7
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2021-04-24 14:52:20 -0300

    Fix Role model

--- src/models/Role.js
+++ src/models/Role.js
@@ -1,12 +1,8 @@
-class Notice {
-    constructor(title, body, date, author, subject, id = null) {
-        this.title = title;
-        this.body = body;
-        this.date = date;
-        this.author = author;
-        this.subject = subject;
+class Role {
+    constructor(name, id = null) {
+        this.name = name;
         this.id = id;
     }
 }
 
-module.exports = Notice;
+module.exports = Role;
